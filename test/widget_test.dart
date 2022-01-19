6bc905a5b06423882adb2446ff4ac0a53447eebe
CraigFoote <CraigFoote@gmail.com>
2022-01-19 13:35:51 -0500

    test cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,20 +10,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:temperature_converter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Test TextFields displayed', (WidgetTester tester) async {
     await tester.pumpWidget(const TemperatureConverterApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Celsius'), findsOneWidget);
+    expect(find.text('Fahrenheit'), findsOneWidget);
+    expect(find.text('Kelvin'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    var textFields = find.byType(TextField);
+    expect(textFields, findsNWidgets(3));
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test celsius entered', (WidgetTester tester) async {
+    await tester.pumpWidget(const TemperatureConverterApp());
+
+    var textFields = find.byType(TextField);
+    await tester.enterText(textFields.at(0), '0');
+    tester.pump();
+    expectSync(find.text('32.0'), findsOneWidget);
+    expectSync(find.text('273.15'), findsOneWidget);
+  });
+
+  testWidgets('Test fahrenheit entered', (WidgetTester tester) async {
+    await tester.pumpWidget(const TemperatureConverterApp());
+
+    var textFields = find.byType(TextField);
+    await tester.enterText(textFields.at(1), '0');
+    tester.pump();
+    expectSync(find.text('-17.77777777777778'), findsOneWidget);
+    expectSync(find.text('255.3722222222222'), findsOneWidget);
+  });
+
+  testWidgets('Test kelvin entered', (WidgetTester tester) async {
+    await tester.pumpWidget(const TemperatureConverterApp());
+
+    var textFields = find.byType(TextField);
+    await tester.enterText(textFields.at(2), '0');
+    tester.pump();
+    expectSync(find.text('-273.15'), findsOneWidget);
+    expectSync(find.text('-459.66999999999996'), findsOneWidget);
   });
 }
